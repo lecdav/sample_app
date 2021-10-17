bea1ec88bd79c45da7f2214b26b297ae4cdb5709
lecdav <lec.david@gmail.com>
2021-10-17 20:37:01 +0200

    refactor titles with yield

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    @title_name = "Home"
   end
 
   def help
+    @title_name = "Help"
   end
 
   def about
+    @title_name = "About"
   end
 end

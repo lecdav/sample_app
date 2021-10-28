47657d580883570223887ae61fd6405b081c067e
lecdav <lec.david@gmail.com>
2021-10-28 22:31:10 +0200

    add signup test and test_helper method

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,5 +4,6 @@ class UserControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_path
     assert_response :success
+    #assert_select "title", "Sign up #{base_title}"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,8 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def setup
+    @base_title = "| Ruby on Rails Tutorial Sample App"
+  end
+  
 end

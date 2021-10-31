c306c81b2bbe6da60bfd892c194145cc6926c841
lecdav <lec.david@gmail.com>
2021-10-31 23:10:23 +0100

    add validation for email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
+
 class User < ApplicationRecord
   validates :name, presence: true, 
                    length: { minimum: 1, maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+[a-z\d\-.]+[a-z\d\-]+\.[a-z]+\z/i
   validates :email, presence: true,
-                    length: { minimum: 5, maximum: 255 }
+                    length: { minimum: 5, maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 end

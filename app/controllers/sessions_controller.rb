4e5948d32c64cda89519daae2886750abe34b50e
lecdav <lec.david@gmail.com>
2021-12-01 22:44:54 +0100

    commit procfile + all sessions files

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email].downcase)
+    if user && user.authenticate(session_params[:password])
+      reset_session
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  private
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
+end

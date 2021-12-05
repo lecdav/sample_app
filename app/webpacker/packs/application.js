a78d2ffda7667bc70b7a6e873b88c9eea716dc88
lecdav <lec.david@gmail.com>
2021-12-05 21:14:39 +0100

    put all assets in app/webpack + run rails webpacker:install

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -3,12 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
-import "jquery";
-import "bootstrap";
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+
+import "../js/bootstrap_js_files.js";
 
 Rails.start();
 Turbolinks.start();

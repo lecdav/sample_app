d5caf26820576e0a4dc13aeaa12ca7502d8b4165
lecdav <lec.david@gmail.com>
2021-12-15 22:40:04 +0100

    create user_digest method in user model

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,7 @@ module SessionsHelper
       @current_user = User.find_by(id: session[:user_id])
     end
   end
+  
   def logged_in?
     !current_user.nil?
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,11 @@ class User < ApplicationRecord
                        length: { minimum: 6, maximum: 30 }
 
   has_secure_password
+
+  # Returns the hash of the given string
+  def user_digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end

9aa6a994537e55f3463732d372ae0efb06970cbb
lecdav <lec.david@gmail.com>
2022-02-04 22:45:05 +0100

    add integreation test for every layout's links

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,15 +5,28 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test 'the truth' do
   #   assert true
   # end
-
+  def setup
+    @user = users(:michael)
+  end
+  
   test 'layout links' do
     get root_path
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
+    # header specific
+      # if not logged in
     assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', login_path
+      # if logged in
+    log_in_as(@user)
+    assert_redirected_to @user
+    get root_path # I don't understand why is it necessary to get root to make the test pass
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', edit_user_path(@user)
+    assert_select 'a[href=?]', logout_path
+    # footer specific
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', "https://news.railstutorial.org/"
   end
-
-
 end

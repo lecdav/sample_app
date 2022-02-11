f404a4199994ef84e7dfbee64b3aee372b6b8b21
lecdav <lec.david@gmail.com>
2022-02-11 22:06:01 +0100

    add the destroy method in the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -42,6 +43,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+   User.find(id: params[:id]).destroy
+   flash[:success] = "User deleted"
+   redirect_to users_path
+  end
+
   private
 
     def user_params
@@ -63,5 +70,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms the user is an admin
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end

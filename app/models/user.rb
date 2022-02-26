35a23b904becdf803e1dc778335e3252c5a1717d
lecdav <lec.david@gmail.com>
2022-02-26 23:23:08 +0100

    send a reset password email in case of fogotten password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
-  
+  attr_accessor :remember_token, :activation_token, :reset_token
+
   # Callback fired before Create or Update
   before_save :downcase_email
   # Callback fired only before Create
@@ -64,9 +64,21 @@ class User < ApplicationRecord
     update_columns(activated: true, activated_at: Time.zone.now)
   end
 
-  # Sends activation email.
+  # Sends activation email
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Creates and assign a password reset token and digest
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token))
+    update_columns(reset_sent_at: Time.zone.now)
+  end
+
+  # Sends password reset email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private

0f2866ae2aad50da1eb74167d86871e1df9747a5
lecdav <lec.david@gmail.com>
2022-03-02 22:03:09 +0100

    add model test for micropost with validation and presence

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    # This code is not idiomatically correct.
+    @micropost = Micropost.new(user_id: @user.id , content: "Lorem Ipsum")
+  end
+
+  test 'micropost should be valid' do
+    assert @micropost.valid?
+  end
+
+  test 'user_id should be present' do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
 end
